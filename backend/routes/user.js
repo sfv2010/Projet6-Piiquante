7144261e222da20f7c8b398e8bcda25e67b20a6a
Sayaka Vincent <sfv2010@hotmail.fr>
2022-07-27 00:50:43 +0200

    fix the comment

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,17 @@
-//Configuration les routes d'authentification
+//---Configuration les routes d'authentification---
 
 const express = require("express");
 const router = express.Router();
-const rateLimit = require("express-rate-limit"); //L’ express-rate-limit est le package npm pour limiter la demande de l’utilisateur.
+const userCtrl = require("../controllers/user");
+const password = require("../middleware/password");
+
+//---L’express-rate-limit est le package npm pour limiter la demande de l’utilisateur---
+const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
     max: 2, // limiter même IP du 2 requests par windowMs
     message: "Trop de requêtes de cette IP",
 });
-
-const userCtrl = require("../controllers/user");
-const password = require("../middleware/password");
 
 router.post("/signup", password, userCtrl.signup);
 router.post("/login", limiter, userCtrl.login);
