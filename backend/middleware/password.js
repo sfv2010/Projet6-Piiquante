7e34e07cd7ef28bd58beced77e8b667cebffe795
Sayaka Vincent <sfv2010@hotmail.fr>
2022-08-05 13:28:01 +0200

    add the comment

--- backend/middleware/password.js
+++ backend/middleware/password.js
@@ -1,8 +1,8 @@
 const passwordSchema = require("../models/password");
 
+//---Exportation (vers routes)---
 module.exports = (req, res, next) => {
     if (passwordSchema.validate(req.body.password)) {
-        //res.status(201).json({ message: "Le mots de passe est fort!" });
         next();
     } else {
         res.status(400).json({ message: "Le mots de passe est faible" });
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,27 +1,29 @@
 //---Configurez le routage---
+
+//---Importation---
+
 const express = require("express");
-const router = express.Router(); //methode express.Router()permet de créer des routeurs séparés pour chaque route principale de votre application
+const router = express.Router(); //methode express.Router()permet de créer des routeurs séparés pour chaque route principale de notre application
 
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config"); // faut placer multer après auth sinon même les images des requêtes non authentifiées seront enregistrées dans le serveur.
 
-const sauceCtrl = require("../controllers/sauce"); //on emporte des routes
+const sauceCtrl = require("../controllers/sauce");
+
+//---Router---
 
 //---Route pour créer un objet ---
 router.post("/", auth, multer, sauceCtrl.createSauce);
 //---Route pour modifier un objet ---
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
-
 //---Route pour supprimer un objet---
 router.delete("/:id", auth, multer, sauceCtrl.deleteSauce);
-
 //---Route pour afficher un seul objet dans la base de donées par son id
 router.get("/:id", auth, sauceCtrl.getOneSauce);
-
 //---Route pour afficher tous les objets Sauce qui sont dans la base de donées
 router.get("/", auth, sauceCtrl.getAllSauce);
-
 //---Route pour lliker ou disliker
 router.post("/:id/like", auth, sauceCtrl.likeOrDislikeSauce);
 
-module.exports = router; // exporter 輸出 書き出す.
+//---Exportation---
+module.exports = router;
