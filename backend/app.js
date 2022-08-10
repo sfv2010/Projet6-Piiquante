5516a8a210967769f0e6a1b60e79fe98b2721a54
Sayaka Vincent <sfv2010@hotmail.fr>
2022-08-11 00:33:16 +0200

    add sanitize

--- backend/app.js
+++ backend/app.js
@@ -8,16 +8,18 @@ const userRoutes = require("./routes/user");
 const path = require("path"); //traiter les requêtes vers la route /image,
 
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 require("dotenv").config();
 
 //--- Sécuriser les requêtes HTML---
 app.use(helmet()); // la methode express app.use s'exécute sur tous les types de requêtes HTTP .
+app.use(mongoSanitize()); //Securiser contre l'injection NoSQL en supprimeant les caractères interdits
 
 //--- Connecter mangooDB---
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("Connexion à MongoDB réussie !"))
-    .catch(() => console.log("Connexion à MongoDB échouée !"));
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 // Pour gérer la requête POST venant de l'application front-end, on a besoin d'en extraire le corps JSON.
 app.use(express.json());
@@ -26,14 +28,17 @@ app.use(express.json());
 //Ici,le middleware ne prend pas d'adresse en premier paramètre, afin de s'appliquer à toutes les routes.
 //Cela permettra à toutes les demandes de toutes les origines d'accéder à notre API
 app.use((req, res, next) => {
-    res.setHeader("Cross-Origin-Resource-Policy", "same-site"); //pour afficher les images en utilisant helmet
-    res.setHeader("Access-Control-Allow-Origin", "*"); // d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    ); //d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc).
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS"); //d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
-    next(); // ne pas oublier de passer next!
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site"); //pour afficher les images en utilisant helmet
+  res.setHeader("Access-Control-Allow-Origin", "*"); // d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  ); //d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc).
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  ); //d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  next(); // ne pas oublier de passer next!
 });
 
 //---Route---
