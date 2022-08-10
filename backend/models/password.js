6ae75be058e379114345c1b22eeb49d336e8c0bf
Sayaka Vincent <sfv2010@hotmail.fr>
2022-08-11 00:33:58 +0200

    change Minimum length

--- backend/models/password.js
+++ backend/models/password.js
@@ -4,22 +4,22 @@ const passwordValidator = require("password-validator");
 const passwordSchema = new passwordValidator();
 
 passwordSchema
-    .is()
-    .min(5) // Minimum length 5
-    .is()
-    .max(100) // Maximum length 100
-    .has()
-    .uppercase() // Must have uppercase letters
-    .has()
-    .lowercase() // Must have lowercase letters
-    .has()
-    .digits(2) // Must have at least 2 digits
-    .has()
-    .not()
-    .spaces() // Should not have spaces
-    .is()
-    .not()
-    .oneOf(["Passw0rd", "Password123"]);
+  .is()
+  .min(8) // Minimum length 8
+  .is()
+  .max(100) // Maximum length 100
+  .has()
+  .uppercase() // Must have uppercase letters
+  .has()
+  .lowercase() // Must have lowercase letters
+  .has()
+  .digits(2) // Must have at least 2 digits
+  .has()
+  .not()
+  .spaces() // Should not have spaces
+  .is()
+  .not()
+  .oneOf(["Passw0rd", "Password123"]);
 
 //---Exportation (vers middleware)---
 module.exports = passwordSchema;
